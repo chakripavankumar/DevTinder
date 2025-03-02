a0b514ff8643e459a46875c6ec9118d5228c906d
Pavan Kumar <siriyalapavankumar@gmail.com>
2025-03-02 14:03:52 +0530

    automated commit

---
+++ devTinder-fe/src/components/Avatar.jsx
@@ -0,0 +1,11 @@
+export function Avatar() {
+  return (
+    <div>
+      <div className="avatar">
+        <div className="w-13 rounded">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+    </div>
+  );
+}
